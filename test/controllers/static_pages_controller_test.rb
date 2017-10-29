052913fc640341469a61196baaa2e142ed789a2a
Jamie White <jamie.parris.white@gmail.com>
2017-10-28 22:35:29 -0400

    added about page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,12 +3,19 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
+    #get static_pages_home_url
     assert_response :success
   end
 
   test "should get help" do
     get :help
+    #get static_pages_help_url
     assert_response :success
   end
 
+  test "should get about" do
+    get :about
+    #get static_pages_about_url
+    assert_response :success
+  end
 end
